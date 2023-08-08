57e8a11046ca14fa079387bc91ff97fb3a78e295
choiseonkang <com1368@naver.com>
2023-08-08 17:22:47 +0900

    feat : 로컬캐시 공부 프로젝트 설정

---
+++ study-local-cache/src/main/java/com/example/studylocalcache/StudyLocalCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.studylocalcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyLocalCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyLocalCacheApplication.class, args);
+    }
+
+}
---
+++ study-local-cache/src/test/java/com/example/studylocalcache/StudyLocalCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studylocalcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyLocalCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
