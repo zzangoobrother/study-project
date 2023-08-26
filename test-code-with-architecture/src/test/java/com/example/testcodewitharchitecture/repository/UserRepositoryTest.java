eb5fe821dc19494d505d8739590bdb96608fba1c
choiseonkang <com1368@naver.com>
2023-08-26 19:48:56 +0900

    feat : UserRepository 테스트 코드 작성

---
+++ test-code-with-architecture/src/test/java/com/example/testcodewitharchitecture/repository/UserRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.example.testcodewitharchitecture.repository;
+
+import com.example.testcodewitharchitecture.model.UserStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest(showSql = true)
+@Sql("/sql/user-repository-test-data.sql")
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        // when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+
+        // then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByIdAndStatus_로_유저_데이터가_없으면_Optional_empty_를_내려준다() {
+        // when
+        Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+
+        // then
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        // when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("abc@naver.com", UserStatus.ACTIVE);
+
+        // then
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    void findByEmailAndStatus_로_유저_데이터가_없으면_Optional_empty_를_내려준다() {
+        // when
+        Optional<UserEntity> result = userRepository.findByEmailAndStatus("abc@naver.com", UserStatus.PENDING);
+
+        // then
+        assertThat(result.isEmpty()).isTrue();
+    }
+}
