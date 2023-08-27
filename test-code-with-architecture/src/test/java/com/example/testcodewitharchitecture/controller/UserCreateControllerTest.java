860dafcd2029a8993def53ab8ed8915a67c1e82b
choiseonkang <com1368@naver.com>
2023-08-27 17:41:30 +0900

    feat : userCreateController 테스트코드 추가

---
+++ test-code-with-architecture/src/test/java/com/example/testcodewitharchitecture/controller/UserCreateControllerTest.java
@@ -0,0 +1,63 @@
+package com.example.testcodewitharchitecture.controller;
+
+import com.example.testcodewitharchitecture.model.dto.UserCreateDto;
+import com.example.testcodewitharchitecture.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/delete-all--data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+class UserCreateControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @MockBean
+    private JavaMailSender mailSender;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void 사용자는_회원_가입을_할_수_있고_회원가입된_사용자는_PENDING_상태이다() throws Exception {
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+                .email("aaa@naver.com")
+                .nickname("aaa")
+                .address("Pang")
+                .build();
+
+        doNothing().when(mailSender).send(any(SimpleMailMessage.class));
+
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userCreateDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.email").value("aaa@naver.com"))
+                .andExpect(jsonPath("$.nickname").value("aaa"))
+                .andExpect(jsonPath("$.status").value("PENDING"));
+    }
+}
