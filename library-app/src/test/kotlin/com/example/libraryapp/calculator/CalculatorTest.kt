0bcdd834b20523c46ca386bd29122d0392b5455c
choiseonkang <com1368@naver.com>
2023-09-05 19:28:54 +0900

    feat : 나누기 테스트 코드 작성

--- library-app/src/test/kotlin/com/example/libraryapp/calculator/CalculatorTest.kt
+++ library-app/src/test/kotlin/com/example/libraryapp/calculator/CalculatorTest.kt
@@ -5,6 +5,8 @@ fun main() {
     calculatorTest.addTest2()
     calculatorTest.minusTest()
     calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
+    calculatorTest.divideExceptionTest()
 }
 
 internal class CalculatorTest {
@@ -57,5 +59,37 @@ internal class CalculatorTest {
         if (calculator.number != 15) {
             throw IllegalStateException()
         }
+    }
+
+    fun divideTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun divideExceptionTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            if (e.message != "0으로 나눌 수 없습니다.") {
+                throw IllegalStateException("메시지가 다릅니다")
+            }
+            return
+        } catch (e: Exception) {
+            throw IllegalStateException()
+        }
+
+        throw IllegalStateException("기대하는 예외가 발생하지 않았습니다.")
     }
 }
