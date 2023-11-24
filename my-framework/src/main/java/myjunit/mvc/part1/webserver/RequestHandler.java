725ddf4fb2b6913f061966c1cf028a0953067b20
choiseonkang <com1368@naver.com>
2023-11-24 22:54:20 +0900

    feat : 기본 main 구성

---
+++ my-framework/src/main/java/myjunit/mvc/part1/webserver/RequestHandler.java
@@ -0,0 +1,46 @@
+package myjunit.mvc.part1.webserver;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class RequestHandler extends Thread {
+    private Socket connection;
+
+    public RequestHandler(Socket connection) {
+        this.connection = connection;
+    }
+
+    public void run() {
+        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            DataOutputStream dos = new DataOutputStream(out);
+            byte[] body = "Hello World".getBytes();
+            response200Header(dos, body.length);
+            responseBody(dos, body);
+        } catch (IOException e) {
+
+        }
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+
+        }
+    }
+
+    private void responseBody(DataOutputStream dos, byte[] body) {
+        try {
+            dos.write(body, 0, body.length);
+            dos.flush();
+        } catch (IOException e) {
+
+        }
+    }
+}
---
+++ my-framework/src/main/java/myjunit/mvc/part1/webserver/WebServer.java
@@ -0,0 +1,30 @@
+package myjunit.mvc.part1.webserver;
+
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+    private static final int DEFAULT_PORT = 8080;
+
+    public static void main(String[] args) throws IOException {
+        int port = 0;
+        if (args == null || args.length == 0) {
+            port = DEFAULT_PORT;
+        } else {
+            port = Integer.parseInt(args[0]);
+        }
+
+        // 서버 소켓을 생성, 웹서버는 기본적으로 8080 포트를 사용한다.
+        try (ServerSocket listenSocket = new ServerSocket(port)) {
+            // 클라이언트가 연결될때까지 대기
+            Socket connection;
+            while ((connection = listenSocket.accept()) != null) {
+                RequestHandler requestHandler = new RequestHandler(connection);
+                requestHandler.start();
+            }
+        }
+    }
+}
