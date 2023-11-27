2bf352c47fd5143de4408c159e9539e9a0cb4a3e
choiseonkang <com1368@naver.com>
2023-11-27 22:39:29 +0900

    docs : 302 status code 적용 구현

--- my-framework/src/main/java/myjunit/mvc/part1/webserver/RequestHandler.java
+++ my-framework/src/main/java/myjunit/mvc/part1/webserver/RequestHandler.java
@@ -49,7 +49,11 @@ public class RequestHandler extends Thread {
                 User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
                 DataBase.addUser(user);
 
-                url = "/index.html";
+                DataOutputStream dos = new DataOutputStream(out);
+                response302Header(dos, "http://localhost:8080/index.html");
+                responseBody(dos, new byte[]{});
+
+                return;
             }
 
             DataOutputStream dos = new DataOutputStream(out);
@@ -66,6 +70,16 @@ public class RequestHandler extends Thread {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String redirectUrl) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: " + redirectUrl + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
 
