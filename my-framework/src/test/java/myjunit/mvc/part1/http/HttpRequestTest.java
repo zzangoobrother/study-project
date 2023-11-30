819f835123e987219298e8adffda7780ceb4c7b3
choiseonkang <com1368@naver.com>
2023-11-30 21:40:09 +0900

    test : HttpRequest Get 방식 테스트 코드 구현

---
+++ my-framework/src/test/java/myjunit/mvc/part1/http/HttpRequestTest.java
@@ -0,0 +1,40 @@
+package myjunit.mvc.part1.http;
+
+import myjunit.test.Assert;
+import myjunit.test.TestCase;
+import myjunit.test.TestResult;
+import myjunit.test.TestSuite;
+import myjunit.test.annotation.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class HttpRequestTest extends TestCase {
+    private String testDirectory = "./my-framework/src/test/resources/";
+
+    public HttpRequestTest(String testCaseName) {
+        super(testCaseName);
+    }
+
+    @Test
+    public void request_get() throws FileNotFoundException {
+        InputStream in = new FileInputStream(new File(testDirectory + "Http_GET.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        Assert.assertEquals("GET", request.getMethod());
+        Assert.assertEquals("/user/create", request.getPath());
+        Assert.assertEquals("keep-alive", request.getHeader("Connection"));
+        Assert.assertEquals("choi", request.getParameter("userId"));
+    }
+
+    public static void main(String[] args) {
+        TestSuite testSuite = new TestSuite(HttpRequestTest.class);
+
+        TestResult testResult = new TestResult();
+        testSuite.run(testResult);
+
+        testResult.printCount();
+    }
+}
