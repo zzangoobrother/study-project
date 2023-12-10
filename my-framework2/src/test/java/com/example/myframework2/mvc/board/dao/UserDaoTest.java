e9b7b0ef0aadf22a9fcd9e5c91a77f189a323e12
choiseonkang <com1368@naver.com>
2023-12-11 00:12:33 +0900

    feat : UserDao 테스트 코드 구현

---
+++ my-framework2/src/test/java/com/example/myframework2/mvc/board/dao/UserDaoTest.java
@@ -0,0 +1,44 @@
+package com.example.myframework2.mvc.board.dao;
+
+import com.example.myframework2.mvc.board.model.User;
+import com.example.myframework2.mvc.core.jdbc.ConnectionManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserDaoTest {
+
+    @BeforeEach
+    public void setup() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("jwp.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+
+    @Test
+    public void crud() throws Exception {
+        User expected = new User("userId", "password", "name", "javajigi@email.com");
+        UserDao userDao = new UserDao();
+        userDao.insert(expected);
+        User actual = userDao.findByUserId(expected.getUserId());
+        assertEquals(expected, actual);
+
+        expected.update(new User("userId", "password2", "name2", "sanjigi@email.com"));
+        userDao.update(expected);
+        actual = userDao.findByUserId(expected.getUserId());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findAll() throws Exception {
+        UserDao userDao = new UserDao();
+        List<User> users = userDao.findAll();
+        assertEquals(1, users.size());
+    }
+}
