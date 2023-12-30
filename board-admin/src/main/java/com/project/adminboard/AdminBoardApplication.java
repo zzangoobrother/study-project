32002978afb9e1ba00a84af199f1b8b05076e75d
choiseonkang <com1368@naver.com>
2023-12-30 18:20:32 +0900

    feat : admin 만들기 연습

---
+++ board-admin/src/main/java/com/project/adminboard/AdminBoardApplication.java
@@ -0,0 +1,9 @@
+package com.project.adminboard;
+
+import org.springframework.boot.SpringApplication;
+
+public class AdminBoardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminBoardApplication.class, args);
+    }
+}
