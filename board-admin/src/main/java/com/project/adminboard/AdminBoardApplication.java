caac90d8f9a6fa05805dbb802988bae2d226018e
choiseonkang <com1368@naver.com>
2023-12-31 15:43:59 +0900

    feat : config 설정

--- board-admin/src/main/java/com/project/adminboard/AdminBoardApplication.java
+++ board-admin/src/main/java/com/project/adminboard/AdminBoardApplication.java
@@ -1,7 +1,11 @@
 package com.project.adminboard;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
+@SpringBootApplication
 public class AdminBoardApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminBoardApplication.class, args);
