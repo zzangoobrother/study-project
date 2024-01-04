d0e5ff966720818d59da0debbf1252b749e6a24b
choiseonkang <com1368@naver.com>
2024-01-05 00:14:56 +0900

    feat : 댓글 관리 controller, service 테스트 코드 작성

---
+++ board-admin/src/main/java/com/project/adminboard/service/ArticleCommentManagementService.java
@@ -0,0 +1,24 @@
+package com.project.adminboard.service;
+
+import com.project.adminboard.dto.ArticleCommentDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ArticleCommentManagementService {
+
+    public List<ArticleCommentDto> getArticleComments() {
+        return List.of();
+    }
+
+    public ArticleCommentDto getArticleComment(Long articleCommentId) {
+        return null;
+    }
+
+    public void deleteArticleComment(Long articleCommentId) {
+
+    }
+}
