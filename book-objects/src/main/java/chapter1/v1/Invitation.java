b7b06503eb6ff8aad03235d7bb7aeb0ce4c5666a
choiseonkang <com1368@naver.com>
2024-01-08 01:10:30 +0900

    feat : docs : 오브젝트 책 챕터1 코드 작성 중 - 1

---
+++ book-objects/src/main/java/chapter1/v1/Invitation.java
@@ -0,0 +1,7 @@
+package chapter1.v1;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ book-objects/src/main/java/chapter1/v1/TicketSeller.java
@@ -0,0 +1,13 @@
+package chapter1.v1;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
