c0448765feefd3a630d1a70e5be3a945d722569c
choiseonkang <com1368@naver.com>
2024-01-19 21:33:10 +0900

    feat : redis sample 프로젝트 세팅

---
+++ price-compare-redis/src/main/java/com/example/pricecompareredis/priceCompareRedisApplication.java
@@ -0,0 +1,11 @@
+package com.example.pricecompareredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class priceCompareRedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(priceCompareRedisApplication.class, args);
+    }
+}
