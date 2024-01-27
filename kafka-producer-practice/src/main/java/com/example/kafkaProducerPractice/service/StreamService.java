4238690a729f16ae6b7d221667fa7a0e466ba9ca
choiseonkang <com1368@naver.com>
2024-01-27 18:28:15 +0900

    feat : kafka streams join 기본 실습

--- kafka-producer-practice/src/main/java/com/example/kafkaProducerPractice/service/StreamService.java
+++ kafka-producer-practice/src/main/java/com/example/kafkaProducerPractice/service/StreamService.java
@@ -4,9 +4,13 @@ import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.JoinWindows;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 public class StreamService {
@@ -14,7 +18,18 @@ public class StreamService {
 
     @Autowired
     public void builedPipeline(StreamsBuilder sb) {
-        KStream<String, String> mystream = sb.stream("fastcampus", Consumed.with(STRING_SERDE, STRING_SERDE));
-        mystream.filter((key, value) -> value.contains("freeClass")).to("freeClassList");
+//        KStream<String, String> mystream = sb.stream("fastcampus", Consumed.with(STRING_SERDE, STRING_SERDE));
+//        mystream.filter((key, value) -> value.contains("freeClass")).to("freeClassList");
+
+        KStream<String, String> leftStream = sb.stream("leftTopic", Consumed.with(STRING_SERDE, STRING_SERDE)).selectKey((k, v) -> v.substring(0, v.indexOf(":")));
+        KStream<String, String> rightStream = sb.stream("rightTopic", Consumed.with(STRING_SERDE, STRING_SERDE)).selectKey((k, v) -> v.substring(0, v.indexOf(":")));
+
+        leftStream.print(Printed.toSysOut());
+        rightStream.print(Printed.toSysOut());
+
+        KStream<String, String> joinedStream = leftStream.join(rightStream, (leftValue, RightValue) -> leftStream + ":" + rightStream, JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofMinutes(1)));
+
+        joinedStream.print(Printed.toSysOut());
+        joinedStream.to("joinedMsg");
     }
 }
