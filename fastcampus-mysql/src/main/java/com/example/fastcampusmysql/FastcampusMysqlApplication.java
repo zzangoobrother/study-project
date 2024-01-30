ea93ed600f3c7ec074ee01107a3b64f2cd1eb398
choiseonkang <com1368@naver.com>
2024-01-31 01:11:05 +0900

    feat : fastcampus mysql 강의 설정

---
+++ fastcampus-mysql/src/main/java/com/example/fastcampusmysql/FastcampusMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.example.fastcampusmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusMysqlApplication.class, args);
+    }
+}
