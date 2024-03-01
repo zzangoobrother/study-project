41913c6fe7ce99391852307587c892be37660e38
choiseonkang <com1368@naver.com>
2024-03-01 20:01:43 +0900

    feat : kafka 실습 구현1

---
+++ fastcampus-kafka/src/main/java/com/example/fastcampuskafka/FastcampusKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.example.fastcampuskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusKafkaApplication.class, args);
+    }
+}
