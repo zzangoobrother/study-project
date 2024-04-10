b6ff9a57a7f018764366978f92d01153656f4a67
choiseonkang <com1368@naver.com>
2024-04-10 14:14:35 +0900

    feat : security study 기본 세팅

---
+++ inflearn-security-study/src/main/java/com/example/inflearnsecuritystudy/inflearnsecuritystudyApplication.java
@@ -0,0 +1,12 @@
+package com.example.inflearnsecuritystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class inflearnsecuritystudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(inflearnsecuritystudyApplication.class, args);
+    }
+}
