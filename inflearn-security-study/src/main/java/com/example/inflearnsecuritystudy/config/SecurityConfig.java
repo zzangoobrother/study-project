50ee73e09fe2de468f16810ae2a28df96a53371a
choiseonkang <com1368@naver.com>
2024-04-12 00:38:48 +0900

    feat : security rememberMe 기능 study

--- inflearn-security-study/src/main/java/com/example/inflearnsecuritystudy/config/SecurityConfig.java
+++ inflearn-security-study/src/main/java/com/example/inflearnsecuritystudy/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.example.inflearnsecuritystudy.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.servlet.http.HttpSession;
 
@@ -13,6 +15,9 @@ import javax.servlet.http.HttpSession;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -20,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated();
 
         http.formLogin()
-                .loginPage("/loginPage")
+//                .loginPage("/loginPage")
                 .defaultSuccessUrl("/")
                 .failureUrl("/login")
                 .usernameParameter("userId")
@@ -44,6 +49,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     session.invalidate();
                 })
                 .logoutSuccessHandler((request, response, authentication) -> response.sendRedirect("/login"))
-                .deleteCookies("remember-me");
+                .deleteCookies("remember-me")
+                .and()
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(3600)
+                .userDetailsService(userDetailsService);
+
+
     }
 }
