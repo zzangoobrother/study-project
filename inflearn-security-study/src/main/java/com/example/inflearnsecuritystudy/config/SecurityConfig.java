1699084f30292852e31bbe40f0ebd46f49b071c6
choiseonkang <com1368@naver.com>
2024-04-13 15:11:18 +0900

    feat : security 권한 설정 기능 study

--- inflearn-security-study/src/main/java/com/example/inflearnsecuritystudy/config/SecurityConfig.java
+++ inflearn-security-study/src/main/java/com/example/inflearnsecuritystudy/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.inflearnsecuritystudy.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,8 +20,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     UserDetailsService userDetailsService;
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN", "SYS", "USER");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest()
                 .authenticated();
 
@@ -60,6 +71,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionFixation()
                 .changeSessionId()
                 .maximumSessions(1)
-                .maxSessionsPreventsLogin(false);
+                .maxSessionsPreventsLogin(true);
     }
 }
