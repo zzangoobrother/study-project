3df349a7df19a0f70dbb2c074e91093f308e55b2
choiseonkang <com1368@naver.com>
2024-04-15 23:16:09 +0900

    feat : security 다중 설정 기능 study

--- inflearn-security-study/src/main/java/com/example/inflearnsecuritystudy/config/SecurityConfig.java
+++ inflearn-security-study/src/main/java/com/example/inflearnsecuritystudy/config/SecurityConfig.java
@@ -3,28 +3,22 @@ package com.example.inflearnsecuritystudy.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDeniedException;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
 import org.springframework.security.web.savedrequest.SavedRequest;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 @Slf4j
 @Configuration
 @EnableWebSecurity
+@Order(1)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -91,5 +85,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .changeSessionId()
                 .maximumSessions(1)
                 .maxSessionsPreventsLogin(true);
+    }
+}
+
+@Configuration
+@Order(0)
+class SecurityConfig2 extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/other/**")
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
     }
 }
