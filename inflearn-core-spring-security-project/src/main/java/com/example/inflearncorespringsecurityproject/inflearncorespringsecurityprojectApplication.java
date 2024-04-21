5bb4af0ad6108ab4a19390eb5549e9e1b82404f0
choiseonkang <com1368@naver.com>
2024-04-21 10:14:25 +0900

    feat : 스프링 시큐리티 강의 기본 프로젝트 세팅

---
+++ inflearn-core-spring-security-project/src/main/java/com/example/inflearncorespringsecurityproject/inflearncorespringsecurityprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.inflearncorespringsecurityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class inflearncorespringsecurityprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(inflearncorespringsecurityprojectApplication.class, args);
+    }
+}
