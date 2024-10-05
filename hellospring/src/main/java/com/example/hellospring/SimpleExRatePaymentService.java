28779f9b1d0770dd07171f7d2c53c008b8b9a9f3
choiseonkang <com1368@naver.com>
2024-10-05 18:03:09 +0900

    feat : 인프런 토비의 스프링6 - 상속을 통해 유연한 확장

---
+++ hellospring/src/main/java/com/example/hellospring/SimpleExRatePaymentService.java
@@ -0,0 +1,15 @@
+package com.example.hellospring;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class SimpleExRatePaymentService extends PaymentService {
+    @Override
+    BigDecimal getExrate(String currency) throws IOException {
+        if (currency.equals("USD")) {
+            return BigDecimal.valueOf(1000);
+        }
+
+        throw new IllegalArgumentException("지원되지 않는 통화입니다.");
+    }
+}
---
+++ hellospring/src/main/java/com/example/hellospring/WebApiExRatePaymentService.java
@@ -0,0 +1,28 @@
+package com.example.hellospring;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+public class WebApiExRatePaymentService extends PaymentService {
+    @Override
+    BigDecimal getExrate(String currency) throws IOException {
+        // 환율 가져오기
+        // https://open.er-api.com/v6/latest/USD
+        URL url = new URL("https://open.er-api.com/v6/latest/" + currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String response = br.lines().collect(Collectors.joining());
+        br.close();
+
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+        return data.rates().get("KRW");
+    }
+}
