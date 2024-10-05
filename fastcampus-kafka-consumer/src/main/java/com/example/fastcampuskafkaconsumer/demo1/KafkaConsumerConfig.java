24973f5a0a721fc14a60b98e184722dc0eade0e7
choiseonkang <com1368@naver.com>
2024-10-05 17:09:46 +0900

    feat : 인프런 토비의 스프링6 강의1

--- fastcampus-kafka-consumer/src/main/java/com/example/fastcampuskafkaconsumer/demo1/KafkaConsumerConfig.java
+++ fastcampus-kafka-consumer/src/main/java/com/example/fastcampuskafkaconsumer/demo1/KafkaConsumerConfig.java
@@ -21,7 +21,7 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, .class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(props);
     }
---
+++ hellospring/src/main/java/com/example/hellospring/ExRateData.java
@@ -0,0 +1,13 @@
+package com.example.hellospring;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRateData(
+        String result,
+        Map<String, BigDecimal> rates
+) {
+}
