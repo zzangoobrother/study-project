c91aea1c2ee0132a40f830db4500616417a105a6
choiseonkang <com1368@naver.com>
2024-10-08 00:25:46 +0900

    feat : 인프런 토비의 스프링6 - 도메인 오브젝트 테스

---
+++ hellospring/src/test/java/com/example/hellospring/payment/PaymentTest.java
@@ -0,0 +1,31 @@
+package com.example.hellospring.payment;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+
+class PaymentTest {
+
+    @Test
+    void createPrepared() {
+        Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+
+        Payment payment = Payment.createPrepared(1L, "USD", BigDecimal.TEN, BigDecimal.valueOf(1_000), LocalDateTime.now(clock));
+
+        Assertions.assertThat(payment.getConvertedAmount()).isEqualByComparingTo(BigDecimal.valueOf(10_000));
+        Assertions.assertThat(payment.getValidUntil()).isEqualTo(LocalDateTime.now(clock).plusMinutes(30));
+    }
+
+    @Test
+    void isValid() {
+        Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+
+        Payment payment = Payment.createPrepared(1L, "USD", BigDecimal.TEN, BigDecimal.valueOf(1_000), LocalDateTime.now(clock));
+
+        Assertions.assertThat(payment.isValid(clock)).isTrue();
+        Assertions.assertThat(payment.isValid(Clock.offset(clock, Duration.of(30, ChronoUnit.MINUTES)))).isFalse();
+    }
+}
