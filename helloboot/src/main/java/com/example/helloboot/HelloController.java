33b7aec201749e892b67ae30ee8996d25eb840f4
choiseonkang <com1368@naver.com>
2024-10-14 21:32:47 +0900

    feat : 인프런 토비의 스프링 부트 - 프로젝트 생성

---
+++ helloboot/src/main/java/com/example/helloboot/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.helloboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name) {
+        return "Hello " + name;
+    }
+}
