e936a0b97569fb1f69e169834bda2d5119e8c269
choiseonkang <com1368@naver.com>
2024-10-15 23:00:25 +0900

    feat : 인프런 토비의 스프링 부트 - 서블릿 컨테이너 요청 구현

--- helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
@@ -1,11 +1,32 @@
 package com.example.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import java.io.IOException;
+
 public class HellobootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    String name = req.getParameter("name");
+
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello " + name);
+                }
+            }).addMapping("/hello");
+        });
+        webServer.start();
     }
 }
