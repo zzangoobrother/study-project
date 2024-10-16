04ad6ef5a54afbd90049021dd6751996b6872bea
choiseonkang <com1368@naver.com>
2024-10-17 01:16:15 +0900

    feat : 인프런 토비의 스프링 부트 - 어노테이션을 활용한 매핑

--- helloboot/src/main/java/com/example/helloboot/HelloController.java
+++ helloboot/src/main/java/com/example/helloboot/HelloController.java
@@ -1,9 +1,13 @@
 package com.example.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
@@ -12,6 +16,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(@RequestParam(name = "name") String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
