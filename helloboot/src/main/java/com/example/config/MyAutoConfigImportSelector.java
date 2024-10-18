de40d375d228e61cad13f33026ba197b387b47ae
choiseonkang <com1368@naver.com>
2024-10-18 18:17:15 +0900

    feat : 인프런 토비의 스프링 부트 - 자동 구성 애노테이션 적용 및 테스트 코드 작성

---
+++ helloboot/src/main/java/com/example/config/MyAutoConfigImportSelector.java
@@ -0,0 +1,26 @@
+package com.example.config;
+
+import org.springframework.boot.context.annotation.ImportCandidates;
+import org.springframework.context.annotation.DeferredImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    @Override
+    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+        List<String> autoConfigs = new ArrayList<>();
+        for(String candidate : ImportCandidates.load(MyAutoConfiguration.class, classLoader)) {
+            autoConfigs.add(candidate);
+        }
+
+        return autoConfigs.stream().toArray(String[]::new);
+    }
+}
---
+++ helloboot/src/main/java/com/example/config/autoconfig/DispatcherServletConfig.java
@@ -0,0 +1,13 @@
+package com.example.config.autoconfig;
+
+import com.example.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@MyAutoConfiguration
+public class DispatcherServletConfig {
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+}
