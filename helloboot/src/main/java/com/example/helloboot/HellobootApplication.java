00304c6b3bb308a6fa83a84b84063cc30ac669a3
choiseonkang <com1368@naver.com>
2024-10-18 10:26:16 +0900

    refactor : 인프런 토비의 스프링 부트 - ApplicationContext, Tomcat 실행 부분 리팩토링

--- helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
@@ -1,32 +1,27 @@
 package com.example.helloboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
 @ComponentScan
 public class HellobootApplication {
 
-    public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
 
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this))
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-        applicationContext.register(HellobootApplication.class);
-        applicationContext.refresh();
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    public static void main(String[] args) {
+        MySpringApplication.run(HellobootApplication.class, args);
     }
 }
