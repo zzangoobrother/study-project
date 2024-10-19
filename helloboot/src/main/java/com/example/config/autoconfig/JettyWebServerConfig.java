19bf8d60e2e3aa08f88bbb56268ded0082edff79
choiseonkang <com1368@naver.com>
2024-10-19 11:45:14 +0900

    feat : 인프런 토비의 스프링 부트 - Jetty 서버 구성 추

--- helloboot/src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
+++ helloboot/src/main/java/com/example/config/autoconfig/JettyWebServerConfig.java
@@ -1,7 +1,7 @@
 package com.example.config.autoconfig;
 
 import com.example.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Condition;
@@ -10,17 +10,17 @@ import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.TomcatCondition.class)
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory")
+@Conditional(JettyWebServerConfig.JettyCondition.class)
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 
-    static class TomcatCondition implements Condition {
+    static class JettyCondition implements Condition {
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
+            return true;
         }
     }
 }
