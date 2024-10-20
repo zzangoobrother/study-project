9a57ab4c1c04a800e03c57f61a1d5bab1ac54a87
choiseonkang <com1368@naver.com>
2024-10-20 09:42:40 +0900

    feat : 인프런 토비의 스프링 부트 - 커스텀 @Conditional 구현 및 적

---
+++ helloboot/src/main/java/com/example/config/ConditionalMyOnClass.java
@@ -0,0 +1,15 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Conditional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Conditional(MyOnClassCondition.class)
+public @interface ConditionalMyOnClass {
+    String value();
+}
