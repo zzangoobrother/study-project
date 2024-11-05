92630636a2d59718d3cc97b0473bdd0b4b66385f
choiseonkang <com1368@naver.com>
2024-11-05 16:06:00 +0900

    feat : 패스트 캠퍼스 Redis 학습 - Strings, list, set, hash 실습

---
+++ redis-test/src/main/java/com/example/ListTest.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.List;
+
+public class ListTest {
+
+    public static void main(String[] args) {
+        try (JedisPool jedisPool = new JedisPool("localhost", 6379)) {
+            try (Jedis jedis = jedisPool.getResource()) {
+                // list
+                // stack
+                jedis.rpush("stack1", "aaaa");
+                jedis.rpush("stack1", "bbbb");
+                jedis.rpush("stack1", "cccc");
+
+//                List<String> stack1 = jedis.lrange("stack1", 0, -1);
+//                stack1.forEach(System.out::println);
+
+                System.out.println(jedis.rpop("stack1"));
+                System.out.println(jedis.rpop("stack1"));
+                System.out.println(jedis.rpop("stack1"));
+
+                // queue
+                jedis.rpush("queue2", "aaaa");
+                jedis.rpush("queue2", "bbbb");
+                jedis.rpush("queue2", "cccc");
+
+                System.out.println(jedis.lpop("queue2"));
+                System.out.println(jedis.lpop("queue2"));
+                System.out.println(jedis.lpop("queue2"));
+
+                // block brpop, blpop
+                while (true) {
+                    List<String> blpop = jedis.blpop(10, "queue:blocking");
+                    if (blpop != null) {
+                        blpop.forEach(System.out::println);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ redis-test/src/main/java/com/example/SetTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.Set;
+
+public class SetTest {
+    public static void main(String[] args) {
+        try (JedisPool jedisPool = new JedisPool("localhost", 6379)) {
+            try (Jedis jedis = jedisPool.getResource()) {
+                jedis.sadd("users:500:follow", "100", "200", "300");
+                jedis.srem("users:500:follow", "100");
+
+                Set<String> smembers = jedis.smembers("users:500:follow");
+                smembers.forEach(System.out::println);
+
+                System.out.println(jedis.sismember("users:500:follow", "200"));
+                System.out.println(jedis.sismember("users:500:follow", "100"));
+
+                System.out.println(jedis.scard("users:500:follow"));
+
+                jedis.sadd("users:100:follow", "300", "400", "500");
+                System.out.println(jedis.sinter("users:500:follow", "users:100:follow"));
+            }
+        }
+    }
+}
