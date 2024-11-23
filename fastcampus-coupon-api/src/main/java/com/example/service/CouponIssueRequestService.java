489d688c4d1b5dae6862bc84d156e514ed73a6ff
choiseonkang <com1368@naver.com>
2024-11-23 17:09:40 +0900

    feat : 패스트 캠퍼스 선착순 쿠폰 시스템 학습 - 쿠폰 발급 mysql xLock 동시성 구현

--- fastcampus-coupon-api/src/main/java/com/example/service/CouponIssueRequestService.java
+++ fastcampus-coupon-api/src/main/java/com/example/service/CouponIssueRequestService.java
@@ -15,9 +15,7 @@ public class CouponIssueRequestService {
     private final DistributeLockExecutor distributeLockExecutor;
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-        distributeLockExecutor.execute("lock_" + requestDto.couponId(), 10000, 10000, () -> {
-            couponIssueService.issue(requestDto.couponId(), requestDto.userId());
-        });
+        couponIssueService.issue(requestDto.couponId(), requestDto.userId());
         log.info("쿠폰 발급 완료, couponId : %s, userId : %s".formatted(requestDto.couponId(), requestDto.userId()));
     }
 }
--- fastcampus-coupon-core/src/main/java/com/example/repository/mysql/CouponJpaRepository.java
+++ fastcampus-coupon-core/src/main/java/com/example/repository/mysql/CouponJpaRepository.java
@@ -1,7 +1,16 @@
 package com.example.repository.mysql;
 
 import com.example.model.Coupon;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface CouponJpaRepository extends JpaRepository<Coupon, Long > {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select c from Coupon c where c.id = :id")
+    Optional<Coupon> findCouponWithLock(long id);
 }
