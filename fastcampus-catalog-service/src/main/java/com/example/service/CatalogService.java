d35244f51b6b1e0e750d001b179e2a1cda60f7b2
choiseonkang <com1368@naver.com>
2024-12-30 21:14:06 +0900

    feat : 패스트 캠퍼스 EDA 주문 카탈로그 kafka 적용 구현

--- fastcampus-catalog-service/src/main/java/com/example/service/CatalogService.java
+++ fastcampus-catalog-service/src/main/java/com/example/service/CatalogService.java
@@ -3,9 +3,9 @@ package com.example.service;
 import com.example.cassandra.entity.Product;
 import com.example.cassandra.repository.ProductRepository;
 import com.example.dto.ProductTagsDto;
-import com.example.feign.SearchClient;
 import com.example.mysql.entity.SellerProduct;
 import com.example.mysql.repository.SellerProductRepository;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,12 +17,12 @@ public class CatalogService {
 
     private final ProductRepository productRepository;
     private final SellerProductRepository sellerProductRepository;
-    private final SearchClient searchClient;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public CatalogService(ProductRepository productRepository, SellerProductRepository sellerProductRepository, SearchClient searchClient) {
+    public CatalogService(ProductRepository productRepository, SellerProductRepository sellerProductRepository, KafkaTemplate<String, Object> kafkaTemplate) {
         this.productRepository = productRepository;
         this.sellerProductRepository = sellerProductRepository;
-        this.searchClient = searchClient;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public Product registerProduct(Long sellerId, String name, String description, Long price, Long stockCount, List<String> tags) {
@@ -32,7 +32,7 @@ public class CatalogService {
         Product product = new Product(sellerProduct.getId(), sellerId, name, description, price, stockCount, tags);
 
         ProductTagsDto dto = new ProductTagsDto(product.getId(), tags);
-        searchClient.addTagCache(dto);
+        kafkaTemplate.send("product_tags_added", dto);
 
         return productRepository.save(product);
     }
@@ -41,7 +41,7 @@ public class CatalogService {
         Optional<Product> product = productRepository.findById(productId);
         if (product.isPresent()) {
             ProductTagsDto dto = new ProductTagsDto(productId, product.get().getTags());
-            searchClient.removeTagCache(dto);
+            kafkaTemplate.send("product_tags_removed", dto);
         }
 
         productRepository.deleteById(productId);
