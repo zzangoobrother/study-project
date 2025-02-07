f430309d615851c3ce536439ea2d35a65c10a422
choiseonkang <com1368@naver.com>
2025-02-07 21:06:46 +0900

    feat : fcm 실패 처리 재전송 구현 queue 생성

--- fcm-project/src/main/java/com/example/after/queue/Queue.java
+++ fcm-project/src/main/java/com/example/after/queue/Queue.java
@@ -3,6 +3,7 @@ package com.example.after.queue;
 import com.example.dto.FcmMessage;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -10,5 +11,11 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class Queue {
     private static final BlockingQueue<FcmMessage> queue = new LinkedBlockingQueue<>();
 
-    
+    public static void add(FcmMessage message) {
+        queue.add(message);
+    }
+
+    public static void addAll(List<FcmMessage> messages) {
+        queue.addAll(messages);
+    }
 }
