26690912685cdcce051b1a30908bb9efd55e0c68
choiseonkang <com1368@naver.com>
2025-06-07 13:42:19 +0900

    feat : session redis 저장 구현

--- chat-system-security-session/src/main/java/com/example/auth/MessageUserDetails.java
+++ chat-system-security-session/src/main/java/com/example/auth/MessageUserDetails.java
@@ -1,5 +1,6 @@
 package com.example.auth;
 
+import com.fasterxml.jackson.annotation.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -7,13 +8,16 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MessageUserDetails implements UserDetails {
 
     private final Long userId;
     private final String username;
     private final String password;
 
-    public MessageUserDetails(Long userId, String username, String password) {
+    @JsonCreator
+    public MessageUserDetails(@JsonProperty("userId") Long userId, @JsonProperty("username") String username, @JsonProperty("password") String password) {
         this.userId = userId;
         this.username = username;
         this.password = password;
@@ -34,6 +38,7 @@ public class MessageUserDetails implements UserDetails {
     }
 
     @Override
+    @JsonIgnore
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
     }
