b25dd0d959023d28a36eac3afdba4da90b580333
choiseonkang <com1368@naver.com>
2025-07-26 21:10:20 +0900

    feat : 인프런 - 클린 스프링, 기본 설정 및 도메인 모델 정리

---
+++ splearn-study/src/main/java/com/example/SplearnStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplearnStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplearnStudyApplication.class, args);
+    }
+
+}
