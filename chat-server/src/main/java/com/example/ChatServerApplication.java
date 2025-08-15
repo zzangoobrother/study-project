b61c244deed7c0df7ec9b2bab108a95be45ef81a
choiseonkang <com1368@naver.com>
2025-08-15 21:02:12 +0900

    feat : chat server 기본 설정

---
+++ chat-server/src/main/java/com/example/ChatServerApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatServerApplication.class, args);
+    }
+}
